2c7ab098340cf7fb54bb1aa1b87db16a78e1d818
sergyobarrus <sergyobarrus@hotmail.com>
2013-06-23 23:35:48 -0300

    Atualizacao ProjetoMD.java (Gnome)

--- src/Conjuntos/ProjetoMD.java
+++ src/Conjuntos/ProjetoMD.java
@@ -1,14 +1,14 @@
-/* ProjetoMD (Main) > Sérgio Barros - Matemática Discreta/2013 */
+/* ProjetoMD (Main) > Sérgio Barros / Henryque Cerqueira - Matemática Discreta/2013 */
+
 package Conjuntos;
 
-import java.util.ArrayList;
 import javax.swing.JOptionPane;
 
 public class ProjetoMD {
 
-        static Conjuntos<String> a = new Conjuntos<String>("A");
-        static Conjuntos<String> b = new Conjuntos<String>("B");
-        
+    static Conjuntos<String> a = new Conjuntos<String>("A");
+    static Conjuntos<String> b = new Conjuntos<String>("B");
+
     public static void main(String[] args) {
         String tipo;
         int op;
@@ -18,6 +18,7 @@ public class ProjetoMD {
                     + "\n[2] - União"
                     + "\n[3] - Intersecção"
                     + "\n[4] - Diferença"
+                    + "\n[5] - Gnome Sort"
                     + "\n[0] - Sair"));
             switch (op) {
                 case 1: // Lancamento
@@ -34,6 +35,10 @@ public class ProjetoMD {
                 case 4: // Diferença
                     diferenca();
                     break;
+
+                case 5: //  Ordenação com GnomeSort
+                    gnomeSort();
+                    break;
             }
         } while (op != 0);
     }
@@ -44,40 +49,53 @@ public class ProjetoMD {
         a.Add("3");
         a.Add("4");
         a.Print();
-
         b.Add("3");
         b.Add("4");
         b.Add("5");
         b.Add("6");
         b.Print();
-        
-/*      a.Add("a");
-        a.Add("b");
-        a.Add("c");
-        a.Add("d");
-        a.Print();
 
-        b.Add("d");
-        b.Add("e");
-        b.Add("f");
-        b.Add("g");
-        b.Print();
-*/
+        /*a.Add("a"); a.Add("b"); a.Add("c"); a.Add("d"); a.Print();
+         b.Add("d"); b.Add("e"); b.Add("f"); b.Add("g"); b.Print();
+         */
     }
 
     private static void uniao() {
         Conjuntos<String> uniao = a.Uniao(b);
-            uniao.Print();
+        uniao.Print();
     }
-    
+
     private static void diferenca() {
         Conjuntos<String> diferenca = a.Diferenca(b);
-            diferenca.Print();
+        diferenca.Print();
     }
+
     private static void interseccao() {
         Conjuntos<String> interseccao = a.Interseccao(b);
-            interseccao.Print();
+        interseccao.Print();
     }
-    
     //Outras verificações da classe Conjuntos ...
+
+    private static void gnomeSort() {
+        String aux = "";
+        int[] vetor = new int[10];
+        for (int i = 0; i < 10; i++) {
+            vetor[i] = (int) (Math.random() * 10);
+
+            if (i != vetor.length - 1) { //Verifica se é o ultimo elemento do vetor
+                aux += vetor[i] + ", ";//armazena em aux o valor da posição do vetor + ", "
+            } else {
+                aux += vetor[i]; //Corrige o bug da casa vazia
+            }
+        }
+        System.out.println("Entrada = [" + aux + "]");
+
+        a.AdiconarOrd(vetor);
+        a.GnomeSort();
+        //a.ImprimirOrd();
+    }
+
+    public static void Imprimir(Object result) {
+        System.out.println(result);
+    }
 }
